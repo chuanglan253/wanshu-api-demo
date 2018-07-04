71691d1cc0f1263a9377a53ceb8968c7c7ba7cdb
huanghe <huanghe@253.com>
2018-07-04 14:55:52 +0800

    JAVA demo for V1.0.5

---
+++ wanshu-api-demo-java/src/main/java/com/chuanglan/wanshu/api/demo/ShangShiXinXi/ShiDaGuDongApi.java
@@ -0,0 +1,54 @@
+package com.chuanglan.wanshu.api.demo.ShangShiXinXi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.chuanglan.wanshu.api.demo.HttpUtils;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * 十大股东接口样例
+ */
+public class ShiDaGuDongApi {
+
+	private static String APP_ID = "qqqqqqqq";
+
+    private static String APP_KEY = "qqqqqqqq";
+
+    private static String API_URL = "https://api.253.com/open/stock/ten-shareholders";
+
+    private static JsonParser jsonParser = new JsonParser();
+
+	public static void main(String[] args) {
+        // 1.调用api
+		JsonObject jsonObject = invokeFactorVerify();
+    	// 2.处理返回结果
+        if (jsonObject != null) {
+            //响应code码。200000：成功，其他失败
+            String code = jsonObject.get("code").getAsString();
+            if ("200000".equals(code) && jsonObject.get("data") != null) {
+                String content = jsonObject.get("data").toString();
+                System.out.println("调用接口成功,content is :" + content);
+            } else {
+                // 记录错误日志，正式项目中请换成log打印
+                System.out.println("调用接口失败,code:" + code + ",msg:" + jsonObject.get("message").getAsString());
+            }
+        }
+
+	}
+	
+	private static JsonObject invokeFactorVerify() {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("appId", APP_ID);
+        params.put("appKey", APP_KEY);
+        params.put("companyKey", "**有限责任公司"); //名称
+        params.put("keyType", "1");//1-公司名、2-公司key
+	    params.put("year", "2018");//年度
+		params.put("quarter", "1");//季度
+        String result = HttpUtils.post(API_URL, params);
+        // 解析json,并返回结果
+        return jsonParser.parse(result).getAsJsonObject();
+    }
+
+}
--- wanshu-api-demo-java/src/main/java/com/chuanglan/wanshu/api/demo/YunYingShangSanYaoSuApi.java
+++ wanshu-api-demo-java/src/main/java/com/chuanglan/wanshu/api/demo/YunYingShangSanYaoSuApi.java
@@ -1,5 +1,6 @@
 package com.chuanglan.wanshu.api.demo;
 
+import com.chuanglan.wanshu.api.demo.HttpUtils;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
@@ -7,46 +8,47 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 身份二要素认证
+ * 运营商三要素认证
  */
-public class ShenfenApi {
+public class YunYingShangSanYaoSuApi {
 
     private static String APP_ID_SHENFEN = "qqqqqqqq";
 
     private static String APP_KEY_SHENFEN = "qqqqqqqq";
 
-    private static String API_URL_SHENFEN = "https://api.253.com/open/idcard/id-card-auth";
+    private static String API_URL_SHENFEN = "https://api.253.com/open/carriers/carriers-auth";
 
     private static JsonParser jsonParser = new JsonParser();
 
     public static void main(String[] args) {
         // 1.调用身份信息校验api
-        final JsonObject jsonObject = ShenfenApi.invokeShenfen("李*", "11010120710101****");
+        final JsonObject jsonObject = invokeShenfen();
 
         // 2.处理返回结果
         if (jsonObject != null) {
             //响应code码。200000：成功，其他失败
             String code = jsonObject.get("code").getAsString();
             if ("200000".equals(code) && jsonObject.get("data") != null) {
-                // 调用身份信息校验成功
+                // 调用成功
                 // 解析结果数据，进行业务处理
                 // 校验状态码  000000：成功，其他失败
                 String value = jsonObject.get("data").getAsJsonArray().get(0).getAsJsonObject().get("value").getAsString();
-                System.out.println("调用身份信息校验成功,value:" + value);
+                System.out.println("调用成功,value:" + value);
             } else {
                 // 记录错误日志，正式项目中请换成log打印
-                System.out.println("调用身份信息校验失败,code:" + code + ",msg:" + jsonObject.get("message").getAsString());
+                System.out.println("调用失败,code:" + code + ",msg:" + jsonObject.get("message").getAsString());
             }
         }
     }
 
 
-    private static JsonObject invokeShenfen(String name, String idNum) {
+    private static JsonObject invokeShenfen() {
         Map<String, String> params = new HashMap<String, String>();
         params.put("appId", APP_ID_SHENFEN);
         params.put("appKey", APP_KEY_SHENFEN);
-        params.put("name", name);
-        params.put("idNum", idNum);
+        params.put("name", "李*");
+        params.put("idNum", "11010120710101****");
+		params.put("mobile", "186****2102");
         String result = HttpUtils.post(API_URL_SHENFEN, params);
         // 解析json,并返回结果
         return jsonParser.parse(result).getAsJsonObject();
