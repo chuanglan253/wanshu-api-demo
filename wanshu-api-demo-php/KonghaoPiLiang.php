82b59832cdc5e95814712bc0227abc037893ffde
huanghe <huanghe@253.com>
2018-08-01 21:27:04 +0800

    PHP's demo for V1.0.6.2

---
+++ wanshu-api-demo-php/KonghaoPiLiang.php
@@ -0,0 +1,19 @@
+<?php
+
+	$url = 'https://api.253.com/open/unn/batch-ucheck';
+	$params = [
+		'appId' => 'xxx', // appId,登录万数平台查看
+		'appKey' => 'xxx', // appKey,登录万数平台查看
+		'mobiles' => '', // 要检测的手机号，多个手机号码用英文半角逗号隔开
+	];
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+	$result = curl_exec($ch);
+	var_dump($result);
+	exit;
