a837760f07f68b9fed71d973b3eeac9db454dad0
huanghe <huanghe@253.com>
2018-08-01 16:07:57 +0800

    JAVA demo for V1.0.6.2

---
+++ wanshu-api-demo-java/src/main/java/com/chuanglan/wanshu/api/demo/KonghaoPiLiangApi.java
@@ -0,0 +1,53 @@
+package com.chuanglan.wanshu.api.demo;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 空号批量检测
+ */
+public class KonghaoPiLiangApi {
+
+    private static String APP_ID_KONGHAO = "qqqqqqqq";
+
+    private static String APP_KEY_KONGHAO = "qqqqqqqq";
+
+    private static String API_URL_KONGHAO = "https://api.253.com/open/unn/batch-ucheck";
+
+    private static JsonParser jsonParser = new JsonParser();
+
+    public static void main(String[] args) {
+        // 1.调用空号检测api
+        final JsonObject jsonObject = invokeKonghao("1999999***,18888888***");
+
+        // 2.处理返回结果
+        if (jsonObject != null) {
+            //响应code码。200000：成功，其他失败
+            String code = jsonObject.get("code").getAsString();
+            if ("200000".equals(code) && jsonObject.get("data") != null) {
+                // 调用空号检测成功
+                // 解析结果数据，进行业务处理
+                // 检测结果
+                System.out.println("调用空号批量检测成功,data:" + jsonObject.get("data"));
+            } else {
+                // 记录错误日志，正式项目中请换成log打印
+                System.out.println("调用空号批量检测失败,code:" + code + ",msg:" + jsonObject.get("message").getAsString());
+            }
+        }
+    }
+
+
+    private static JsonObject invokeKonghao(String mobiles) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("appId", APP_ID_KONGHAO);
+        params.put("appKey", APP_KEY_KONGHAO);
+        params.put("mobiles",mobiles);
+        String result = HttpUtils.post(API_URL_KONGHAO, params);
+        // 解析json,并返回结果
+        return jsonParser.parse(result).getAsJsonObject();
+    }
+
+}
