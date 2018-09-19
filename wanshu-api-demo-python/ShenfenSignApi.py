08a6c700a466e0ed6c2d32d718879267a88ce48f
huanghe <huanghe@253.com>
2018-09-19 18:52:28 +0800

    添加身份证签名版接口

---
+++ wanshu-api-demo-python/ShenfenSignApi.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from urllib2 import Request, urlopen, URLError
+import urllib
+import json
+
+import hmac
+import hashlib
+import base64
+
+
+# 身份证二要素认证
+
+def post(url, data):
+    params = urllib.urlencode(data)
+    print(u'发起的请求为：%s' % url)
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    headers = {'User-Agent': user_agent, 'Content-type': 'application/x-www-form-urlencoded',
+               'Accept-Charset': 'utf-8'}
+    request = Request(url, params, headers)
+    try:
+        response = urlopen(request, timeout=3)
+    except URLError, e:
+        print u'发送请求失败，原因：', e
+        return None
+    else:
+        return response.read().decode('utf-8')
+
+
+if __name__ == "__main__":
+    invoke_url = 'https://api.253.com/open/idcard/id-card-auth/vs'
+    invoke_data = {'appId': '12345678', 'name': '李**', 'idNum': '62052219940424****'}
+    appKey = '12345678'
+
+    encryptStr = 'appId' + invoke_data['appId'] + 'idNum' + invoke_data['idNum'] + 'name' + invoke_data['name']
+
+    my_sign = hmac.new(appKey, encryptStr, hashlib.sha1).digest()
+    my_sign = base64.b64encode(my_sign)
+
+    invoke_data = {'appId': invoke_data['appId'], 'sign': my_sign, 'name': invoke_data['name'], 'idNum': invoke_data['idNum']}
+
+    # 1. 调用api
+    result_data = post(invoke_url, invoke_data)
+    # 2.处理返回结果
+    result = json.loads(result_data, 'utf-8') if result_data is not None else exit(1)
+    # 响应code码。200000：成功，其他失败
+    if result is None or '200000' != result['code']:
+        print u'调用失败,code:', result['code'], ',msg:', result['message']
+    else:
+        # 调用身份信息检测成功
+        # 解析结果数据，进行业务处理
+        # 检测结果  000000：成功，其他失败
+        print u'调用成功,data:', result['data']
