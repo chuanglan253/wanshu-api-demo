54c8d21c58e0101dccaf46ded8e3f6815fa00fcc
liutao <liut@253.com>
2018-10-18 17:52:41 +0800

    手写OCR接口

---
+++ wanshu-api-demo-csharp/wanshu-api-demo/Ocr/ShouXieOcrApi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace wanshu_api_demo
+{
+	/**
+	 * 手写OCR接口样例
+	 */
+	public class ShouXieOcrApi
+	{
+		const string APP_ID = "qqqqqqqq";
+	    const string APP_KEY = "qqqqqqqq";	
+	    const string API_URL = "https://api.253.com/open/i/ocr/hand-api";
+	
+	    public static void Main() {
+	        // 1.调用api
+	        JObject jsonObject = invoke();
+	
+	        // 2.处理返回结果
+	        if (jsonObject != null) {
+	            //响应code码。200000：成功，其他失败
+	            string code = jsonObject["code"].ToString();
+	            if ("200000".Equals(code) && null != jsonObject["data"]) {
+	                string content = jsonObject["data"].ToString();
+                	Console.WriteLine("查询成功,content is :" + content);
+	            } else {
+	                // 记录错误日志，正式项目中请换成log打印
+	                Console.WriteLine("查询失败,code:" + code + ",msg:" + jsonObject["message"]);
+	            }
+	        }
+	    }
+	
+	
+	    static JObject invoke() {
+	        IDictionary<string, string> dic = new Dictionary<string, string>();
+	        dic.Add("appId", APP_ID);
+	        dic.Add("appKey", APP_KEY);
+	         //imageType为URL时，传入照片的网络URL地址, 支持jpg/png/bmp格式；
+            //imageType为BASE64时，传入照片的base64字符编码，base64编码不包含data:image前缀，且图片大小不能大于2M
+	        dic.Add("image","https://***/bankCard20180531160200.jpg");
+	        dic.Add("imageType", "URL"); //图片类型（图片类型：URL或BASE64）
+	        string result = HttpUtility.Post(API_URL, dic);
+	        // 解析json,并返回结果
+	         return string.IsNullOrEmpty(result) ? null : (JObject)JsonConvert.DeserializeObject(result);
+	    }
+	}
+}
---
+++ wanshu-api-demo-python/Ocr/ShouXieOCR.py
@@ -0,0 +1,47 @@
+﻿#!/usr/bin/env python
+#coding=utf-8
+
+from urllib2 import Request, urlopen, URLError
+import urllib
+import json
+import base64
+
+#手写OCR接口样例
+def post(url, data):
+    params = urllib.urlencode(data)
+    print(u'发起的请求为：%s' % url)
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    headers = {'User-Agent': user_agent, 'Content-type': 'application/x-www-form-urlencoded',
+               'Accept-Charset': 'utf-8'}
+    request = Request(url, params, headers)
+    try:
+        response = urlopen(request, timeout=10)
+    except URLError, e:
+        print u'发送请求失败，原因：', e
+        return None
+    else:
+        return response.read().decode('utf-8')
+
+
+if __name__ == "__main__":
+
+	#要识别的图片
+	imageFile=open(r'c:\image.jpg','rb')	
+	base64Str=base64.b64encode(imageFile.read())
+	imageFile.close()
+	
+	invoke_data = {'appId': '12345678', 'appKey': '12345678', 'imageType': 'BASE64','image': base64Str}
+	invoke_url = 'https://api.253.com/open/i/ocr/hand-api'
+
+    	# 1. 调用api
+    	result_data = post(invoke_url, invoke_data)
+    	# 2.处理返回结果
+    	result = json.loads(result_data) if result_data is not None else exit(1)
+    	# 响应code码。200000：成功，其他失败
+    	if result is None or '200000' != result['code']:
+        	print u'调用失败,code:', result['code'], ',msg:', result['message']
+    	else:
+        	# 调用成功
+        	# 解析结果数据，进行业务处理
+        	# 检测结果
+        	print u'调用成功,result:', result['data']			
