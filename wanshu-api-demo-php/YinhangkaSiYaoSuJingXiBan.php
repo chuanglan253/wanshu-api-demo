76aea322b16c377cb2cff5edeb75cfbd2a31c324
liutao <liut@253.com>
2018-11-27 16:16:59 +0800

    银行卡精细版

---
+++ wanshu-api-demo-php/YinhangkaSiYaoSuJingXiBan.php
@@ -0,0 +1,22 @@
+<?php
+
+    $url = 'https://api.253.com/open/bankcard/card-auth-detail';
+    $params = [
+        'appId' => 'xxx', // appId,登录万数平台查看
+        'appKey' => 'xxx', // appKey,登录万数平台查看
+        'name' => '', // 姓名
+        'idNum' => '', // 身份证号码，限单个
+        'cardNo' => '', // 银行卡号，限单个
+        'mobile' => '' // 银行预留手机号码，限单个，仅支持国内11位号码
+    ];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+    $result = curl_exec($ch);
+    var_dump($result);
+    exit;
