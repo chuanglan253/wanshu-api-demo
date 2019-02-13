f8f5ee3660393a2e3000c8b6c943efd7534c8258
liutao <liut@253.com>
2019-02-13 11:19:50 +0800

    羊毛标签版接口

---
+++ wanshu-api-demo-java/src/main/java/com/chuanglan/wanshu/api/demo/YangMaoDunBiaoQian/YangMaoDunBiaoQianBanApi.java
@@ -0,0 +1,56 @@
+package com.chuanglan.wanshu.api.demo.YangMaoDunBiaoQian;
+
+import com.chuanglan.wanshu.api.demo.HttpUtils;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 羊毛盾标签版接口
+ */
+public class YangMaoDunBiaoQianBanApi {
+
+    private static String APP_ID_YANGMAODANG = "12345678";
+
+    private static String APP_KEY_YANGMAODANG = "12345678";
+
+    private static String API_URL_YANGMAODANG = "https://api.253.com/open/wooltag/wooltag";
+
+    private static JsonParser jsonParser = new JsonParser();
+
+    public static void main(String[] args) {
+        // 1.调用羊毛盾标签版接口api
+        final JsonObject jsonObject = YangMaoDunBiaoQianBanApi.invokeApi("1705003****", "*.*.*.*", "0");
+
+        // 2.处理返回结果
+        if (jsonObject != null) {
+            //响应code码。200000：成功，其他失败
+            String code = jsonObject.get("code").getAsString();
+            if ("200000".equals(code) && jsonObject.get("data") != null) {
+                // 调成功
+                // 解析结果数据，进行业务处理
+                // 检测结果
+                System.out.println("调用羊毛盾标签版接口成功,data:" + jsonObject.get("data").toString());
+            } else {
+                // 记录错误日志，正式项目中请换成log打印
+                System.out.println("调用羊毛盾标签版接口失败,code:" + code + ",msg:" + jsonObject.get("message").getAsString());
+            }
+        }
+    }
+
+
+    private static JsonObject invokeApi(String mobile, String ip,String type) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("appId", APP_ID_YANGMAODANG);
+        params.put("appKey", APP_KEY_YANGMAODANG);
+        params.put("mobile", mobile);
+        params.put("ip", ip);
+        params.put("type", type);
+        String result = HttpUtils.post(API_URL_YANGMAODANG, params);
+        // 解析json,并返回结果
+        return jsonParser.parse(result).getAsJsonObject();
+    }
+
+}
